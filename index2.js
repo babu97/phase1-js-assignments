43324fc3f2888f88394b148d8e2feaef90d8991c
babu97 <benkaimugul@gmail.com>
2023-09-24 21:42:32 +0300

    finished solution 2

--- index2.js
+++ index2.js
@@ -1,10 +1,34 @@
+
+// Write a program that takes the speed of a car as input e.g 80. If the
+//  speed is less than 70,it should print “Ok”. Otherwise, for every 5 km/s
+//   above the speed limit (70), it should give the driver one demerit point
+//    and print the total number of demerit points. For example,
+//     if the speed is 80, it should print: “Points: 2”. If the driver 
+//     gets more than 12 points, the function should print: “License suspended”.
+let  speedInput = document.getElementById("speed")
+let results = document.getElementById("result")
+
 function detectSpeed(){
-    const speed = 0
+    //Declare Variables BasseSpeed to hold the accepted speed and points to keep track of the points
+    let speed = parseFloat(speedInput.value)
+    const baseSpeed = 70
+     let points = 0
+    if (speed <=baseSpeed){
+        results.textContent='Speed Ok';
+    }else {
+       let points = (speed-baseSpeed)/5
+      if (points>12){
+        results.textContent='Your License has been suspended!'
+    
+      }else{
+        results.textContent=`Points: ${points}`
+      }
 
-    if (speed < 70){
 
-    console.log 
     }
-
-
+  
 }
+
+const button= document.getElementById("btn")
+button.addEventListener("click", detectSpeed )
+
---
+++ index3.js
@@ -0,0 +1,5 @@
+// Write a program whose major task is to calculate an individual’s Net Salary 
+// by getting the inputs of basic salary and benefits. 
+// Calculate the payee (i.e. Tax), NHIF Deductions, NSSF Deductions, gross salary, and net salary. 
+
+//input basic pay and add the beni 
